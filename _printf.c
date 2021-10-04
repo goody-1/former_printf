dd84ecfbf70a86eb0e0c3b5f44cbde2ad7921859
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-10-04 10:47:20 +0200

    _printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
  * _printf -produces output according to a format.
@@ -11,11 +9,20 @@
 
 int _printf(const char *format, ...)
 {
+int print_c;
+
+print_t func[] = {
+{"c", print_char},
+{"s", print_string},
+{"%", print_percent}
+}; 
+
 va_list list;
+ if (format == NULL)
+ return (-1);
 
 va_start(list, format);
-
-
+print_c = printer_func(format, func, list);
 va_end(list);
-return (printed_arg);
+return (print_c);
 }
--- main.h
+++ main.h
@@ -1,11 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct printer - defines a structure for symbols and func.
+ * @symbol: the operator for a function
+ * @func: a function
+ */
+
+struct printer
+{
+char *symbol;
+int (*func)(va_list);
+}; typedef struct printer print_t;
+
 /* function that produces output according to a format */
 int _printf(const char *format, ...);
-
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 
 #endif /* MAIN_H */
